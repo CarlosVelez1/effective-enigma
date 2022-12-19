63e23c72cbfe39f2e0fec962eeb65904a39de9f7
CarlosVelez1 <118563281+CarlosVelez1@users.noreply.github.com>
2022-12-19 00:01:12 -0500

    Add files via upload

--- UatSpaceProgram.js
+++ UatSpaceProgram.js
@@ -10,3 +10,15 @@ function countDown()
     }
     setTimeout(function() { alert("Countdown Has Reached The Halfway Point"); }, 5001);
 }
+//With these functions the buttons at the moment can be interchanged
+//between one another to disabled or enabled
+function Start()
+{
+    document.getElementById("Play").disabled = true;
+    document.getElementById("Stop").disabled = false;
+}
+function Pause()
+{
+    document.getElementById("Play").disabled = false;
+    document.getElementById("Stop").disabled = true;
+}
